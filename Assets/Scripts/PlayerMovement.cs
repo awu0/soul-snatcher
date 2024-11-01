bf437adf3d3af0ab29e57cae1907e10feffe0d0c
NovanYang <zy2379@nyu.edu>
2024-11-01 15:42:36 -0400

    Turn System Implemented

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -8,40 +8,55 @@ public class PlayerMovement : MonoBehaviour
     public int startY = 0;
     public int locX;
     public int locY;
+    public int maxActionCount = 10;
+    public int actionCount = 0;
     public GameObject grids;
+    public GameObject turnManager;
 
     private void Start()
     {
         locX = startX;
         locY = startY;
-
+        actionCount = maxActionCount;
     }
 
     private void Update()
     {
+        if (turnManager != null) {
+            if (turnManager.GetComponent<TurnManager>().state == TurnManager.STATES.PLAYER_ROUND) { 
+                DetectForMovement();
+            }
+        }
+    }
+
+    void DetectForMovement() {
         //move up
         if (Input.GetKeyDown(KeyCode.W))
         {
             locY += 1;
             grids.GetComponent<Grids>().HandelPlayerMovement(0, 1);
+            actionCount -= 1;
         }
         //move down
         else if (Input.GetKeyDown(KeyCode.S))
         {
             locY -= 1;
             grids.GetComponent<Grids>().HandelPlayerMovement(0, -1);
+            actionCount -= 1;
         }
         //move left
         else if (Input.GetKeyDown(KeyCode.A))
         {
             locX -= 1;
             grids.GetComponent<Grids>().HandelPlayerMovement(-1, 0);
+            actionCount -= 1;
         }
         //move right
         else if (Input.GetKeyDown(KeyCode.D))
         {
             locX += 1;
             grids.GetComponent<Grids>().HandelPlayerMovement(1, 0);
+            actionCount -= 1;
         }
     }
 }
---
+++ Assets/Scripts/TurnManager.cs
@@ -0,0 +1,75 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TurnManager : MonoBehaviour
+{
+    
+    public enum STATES
+    {
+        ROUND_START,
+        PLAYER_ROUND,
+        PLAYER_ACTION,
+        ENEMY_ROUND,
+        ROUND_END,
+    }
+    public GameObject Player;
+    public STATES state = STATES.ROUND_START;
+    public float pauseDuration = 1f;
+
+    private void Start()
+    {
+        StartCoroutine(RunTurnManager());
+    }
+
+    private IEnumerator RunTurnManager()
+    {
+        while (true)
+        {
+            switch (state)
+            {
+                case STATES.ROUND_START:
+                    //refill action count
+                    Player.GetComponent<PlayerMovement>().actionCount = Player.GetComponent<PlayerMovement>().maxActionCount;
+                    Debug.Log("ROUND START");
+                    state = STATES.PLAYER_ROUND;
+                    yield return new WaitForSeconds(pauseDuration);
+                    break;
+
+                case STATES.PLAYER_ROUND:
+                    Debug.Log("PLAYER ROUND");
+                    //go to next round if player can't action anymore
+                    if (Player != null)
+                    {
+                        if (Player.GetComponent<PlayerMovement>().actionCount <= 0)
+                        {
+                            state = STATES.PLAYER_ACTION;
+                        }
+                    }
+                    else {
+                        state = STATES.PLAYER_ACTION;
+                    }
+                    yield return new WaitForSeconds(pauseDuration);
+                    break;
+
+                case STATES.PLAYER_ACTION:
+                    Debug.Log("PLAYER ACTION");
+                    state = STATES.ENEMY_ROUND;
+                    yield return new WaitForSeconds(pauseDuration);
+                    break;
+
+                case STATES.ENEMY_ROUND:
+                    Debug.Log("ENEMY ROUND");
+                    state = STATES.ROUND_END;
+                    yield return new WaitForSeconds(pauseDuration);
+                    break;
+
+                case STATES.ROUND_END:
+                    Debug.Log("ROUND END");
+                    state = STATES.ROUND_START;
+                    yield return new WaitForSeconds(pauseDuration);
+                    break;
+            }
+        }
+    }
+}
