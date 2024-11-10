37fe47e61fdb27e8164429afe7448cea9eeb17f2
teddyclark <tedclark72@gmail.com>
2024-11-10 14:30:31 -0500

    ability contexts

--- Assets/Scripts/Abilities/PillbugRoll.cs
+++ Assets/Scripts/Abilities/PillbugRoll.cs
@@ -5,9 +5,30 @@ using UnityEngine;
 
 public class PillbugRoll : Ability
 {
-    public override void ActivateAbility()
+    public override AbilityType Type => AbilityType.Directional;
+
+    protected override void ActivateInternal(AbilityContext context)
     {
-        
+        Debug.Log($"{gameObject} used PillbugRoll");
+
+        var directionalContext = (DirectionalContext)context;
+        var (startX, startY) = Caster.GetCurrentPosition();
+
+        Vector2Int current = new Vector2Int(startX, startY) + directionalContext.Direction;
+        Grids grids = directionalContext.Grids;
+
+        while (!grids.IsCellOccupied(current.x, current.y) && grids.IsPositionWithinBounds(current.x, current.y))
+        {
+            Caster.MoveTo(current.x, current.y);
+            current += directionalContext.Direction;
+        }
+
+        // check if the immediate next tile is an Entity, if it is do damage to it
+        if (grids.IsPositionWithinBounds(current.x, current.y) && grids.IsCellOccupied(current.x, current.y))
+        {
+            Entity target = grids.GetEntityAt(current.x, current.y);
+            target.TakeDamage(directionalContext.Damage);
+        }
     }
 
     /// <summary>
@@ -15,27 +36,27 @@ public class PillbugRoll : Ability
     /// </summary>
     /// <param name="grids">The Grids object</param>
     /// <param name="direction">Which way to roll: Vector2Int.up, Vector2Int.down, Vector2Int.left, Vector2Int.right</param>
-    public void ActivateAbility(Grids grids, Vector2Int direction)
-    {
-        Debug.Log($"{gameObject} used PillbugRoll");
+    // public void ActivateAbility(Grids grids, Vector2Int direction)
+    // {
+    //     Debug.Log($"{gameObject} used PillbugRoll");
         
-        // moves continuously in the specified direction until something is hit
-        var (startX, startY) = Caster.GetCurrentPosition();
+    //     // moves continuously in the specified direction until something is hit
+    //     var (startX, startY) = Caster.GetCurrentPosition();
         
-        Vector2Int current = new Vector2Int(startX, startY) + direction;
+    //     Vector2Int current = new Vector2Int(startX, startY) + direction;
         
-        while (!grids.IsCellOccupied(current.x, current.y) && grids.IsPositionWithinBounds(current.x, current.y))
-        {
-            Caster.MoveTo(current.x, current.y);
-            current += direction;
-        }
+    //     while (!grids.IsCellOccupied(current.x, current.y) && grids.IsPositionWithinBounds(current.x, current.y))
+    //     {
+    //         Caster.MoveTo(current.x, current.y);
+    //         current += direction;
+    //     }
         
         
-        // check if the immediate next tile is an Entity, if it is do damage to it
-        if (grids.IsPositionWithinBounds(current.x, current.y) && grids.IsCellOccupied(current.x, current.y))
-        {
-            Entity target = grids.GetEntityAt(current.x, current.y);
-            target.TakeDamage(damage);
-        }
-    }
+    //     // check if the immediate next tile is an Entity, if it is do damage to it
+    //     if (grids.IsPositionWithinBounds(current.x, current.y) && grids.IsCellOccupied(current.x, current.y))
+    //     {
+    //         Entity target = grids.GetEntityAt(current.x, current.y);
+    //         target.TakeDamage(damage);
+    //     }
+    // }
 }
