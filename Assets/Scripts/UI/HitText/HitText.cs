d338a1596180a72839d61bb967f5aae6bb1cc1cf
NovanYang <zy2379@nyu.edu>
2024-11-15 22:18:07 -0500

    Animation Tune

--- Assets/Scripts/UI/HitText/HitText.cs
+++ Assets/Scripts/UI/HitText/HitText.cs
@@ -5,9 +5,12 @@ using UnityEngine;
 public class HitText : MonoBehaviour
 {
     public float destoryTime = 1.5f;
+    public Vector3 offset = new Vector3(0, 3, 0);
 
     private void Start()
     {
         Destroy(gameObject, destoryTime);
+
+        transform.localPosition += offset;
     }
 }
