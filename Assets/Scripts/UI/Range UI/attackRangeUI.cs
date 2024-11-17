9cc4d34a5cfec36664a6139deb2ea10fb2758ca3
NovanYang <zy2379@nyu.edu>
2024-11-16 19:41:21 -0500

    Attack Range Indicator

--- Assets/Scripts/UI/Range UI/attackRangeUI.cs
+++ Assets/Scripts/UI/Range UI/attackRangeUI.cs
@@ -6,23 +6,44 @@ public class attackRangeIO : MonoBehaviour
 {
     public Player player;
     public GameObject rangeUIPrefab;
+    public GameManager manager;
     private int gridX;
     private int gridY;
+
+    private List<GameObject> rangeIndicators = new List<GameObject>();
+
     private void Start()
     {
         if (player == null || rangeUIPrefab == null) return;
 
         Vector3 playerPosition = player.transform.position;
-        (gridX, gridY) = player.GetCurrentPosition();
 
-        GameObject rangeLeft = Instantiate(rangeUIPrefab, new Vector3(-1, 0, 0) + playerPosition, Quaternion.identity);
-        rangeLeft.transform.SetParent(player.transform);
-        GameObject rangeRight = Instantiate(rangeUIPrefab, new Vector3(1, 0, 0) + playerPosition, Quaternion.identity);
-        rangeRight.transform.SetParent(player.transform);
-        GameObject rangeUp = Instantiate(rangeUIPrefab, new Vector3(0, 1, 0) + playerPosition, Quaternion.identity);
-        rangeUp.transform.SetParent(player.transform);  
-        GameObject rangeDown = Instantiate(rangeUIPrefab, new Vector3(0, -1, 0) + playerPosition, Quaternion.identity);
-        rangeDown.transform.SetParent(player.transform);
+        rangeIndicators.Add(CreateRangeIndicator(new Vector3(-1, 0, 0) + playerPosition));
+        rangeIndicators.Add(CreateRangeIndicator(new Vector3(1, 0, 0) + playerPosition));
+        rangeIndicators.Add(CreateRangeIndicator(new Vector3(0, 1, 0) + playerPosition));
+        rangeIndicators.Add(CreateRangeIndicator(new Vector3(0, -1, 0) + playerPosition));
+    }
+
+    private void Update()
+    {
+        if (player != null) {
+            bool showAttackRange = player.selectedAction == Player.SELECTED.ATTACK && manager.state == GameManager.STATES.PLAYER_ROUND;
+            SetRangeIndicatorActivate(showAttackRange);
+        }    
+    }
+
+    private GameObject CreateRangeIndicator(Vector3 position) { 
+        GameObject rangedIndicator = Instantiate(rangeUIPrefab, position, Quaternion.identity);
+        rangedIndicator.transform.SetParent(player.transform);
+        return rangedIndicator;
+    }
+
+    private void SetRangeIndicatorActivate(bool isActive) {
+        foreach (GameObject indicator in rangeIndicators) {
+            if (indicator != null) { 
+                indicator.SetActive(isActive);
+            }
+        }
     }
 
 }
