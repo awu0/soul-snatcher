9411ccfca5a02afb1d3fe1896073c2ad6369c727
Ryan Fleishman <fleish31@gmail.com>
2024-11-16 23:49:08 -0500

    added animator lines to change animation as well
    slime idle would just override the sprite change if not. Still need to make animation for snake and golem so might not do anything there

--- Assets/Scripts/PlayerSpriteChanger.cs
+++ Assets/Scripts/PlayerSpriteChanger.cs
@@ -10,11 +10,13 @@ public class PlayerSpriteChanger : MonoBehaviour {
 
 
   private Dictionary<EntityType, Sprite> entityToSpriteMap;
+  private Dictionary<EntityType, string> entityToAnimMap;
   private SpriteRenderer spriteRenderer;
+  public Animator animator;
 
-  void Start() {
+    void Start() {
     spriteRenderer = GetComponent<SpriteRenderer>();
-
+    animator = GetComponent<Animator>();
     entityToSpriteMap = new Dictionary<EntityType, Sprite> 
     {
       { EntityType.Slime, slimeSprite },
@@ -23,14 +25,29 @@ public class PlayerSpriteChanger : MonoBehaviour {
       { EntityType.Snake, snakeSprite },
       { EntityType.StoneGolem, stoneGolemSprite }
     };
-  }
+
+    entityToAnimMap = new Dictionary<EntityType, string>
+    {
+      { EntityType.Slime, "isSlime" },
+      { EntityType.GiantPillbug, "isBug" },
+      { EntityType.EvilEye, "isEye" },
+      { EntityType.Snake, "isSnake" },
+      { EntityType.StoneGolem, "isGolem" }
+    };
+    }
 
   public void ChangePlayerSprite(EntityType entityType) {
-    if (entityToSpriteMap.TryGetValue(entityType, out Sprite newSprite)) {
-      spriteRenderer.sprite = newSprite;
-      spriteRenderer.color = Color.blue;
-    } else {
-      Debug.LogWarning($"No sprite found for entity type {entityType}");
-    }
-  }
+        animator.SetBool("isBug", false);
+        animator.SetBool("isEye", false);
+        animator.SetBool("isGolem", false);
+        animator.SetBool("isSnake", false);
+
+        if (entityToSpriteMap.TryGetValue(entityType, out Sprite newSprite) && entityToAnimMap.TryGetValue(entityType, out string newAnim)) {
+          spriteRenderer.sprite = newSprite;
+          animator.SetBool(newAnim, true);
+          spriteRenderer.color = Color.blue;
+        } else {
+          Debug.LogWarning($"No sprite found for entity type {entityType}");
+        }
+      }
 }
