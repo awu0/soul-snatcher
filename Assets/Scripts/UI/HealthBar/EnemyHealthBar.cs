22c59977ca57720cc65b91bd101272b13c72fa20
NovanYang <zy2379@nyu.edu>
2024-11-24 16:01:26 -0500

    Adding Enemy HealthBar

---
+++ Assets/Scripts/UI/HealthBar/EnemyHealthBar.cs
@@ -0,0 +1,24 @@
+using Microsoft.Unity.VisualStudio.Editor;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class EnemyHealthBar : MonoBehaviour
+{
+    public Enemy Enemy;
+    public UnityEngine.UI.Image healthBar;
+
+    public float health;
+    public float maxHealth;
+
+    private void Update()
+    {
+        if (Enemy != null && healthBar != null) {
+            health = Enemy.health;
+            maxHealth = Enemy.maxHealth;
+            float fill = health / maxHealth;
+            healthBar.fillAmount = fill; ;
+        }
+    }
+}
