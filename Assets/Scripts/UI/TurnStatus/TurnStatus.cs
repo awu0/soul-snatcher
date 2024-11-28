7daeaaee8dc7b3b35d2c95fff5295c2337a387a3
NovanYang <zy2379@nyu.edu>
2024-11-28 16:36:20 -0500

    Turn Status Completed

---
+++ Assets/Scripts/UI/TurnStatus/TurnStatus.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class TurnStatus : MonoBehaviour
+{
+    public TextMeshProUGUI statusText;
+    public GameManager manager;
+
+    private bool playerTurn = false;
+
+    private void Update()
+    {
+        if (manager != null) {
+            playerTurn = manager.GetComponent<GameManager>().state == GameManager.STATES.PLAYER_ROUND;
+
+            if (statusText != null) {
+                if (playerTurn)
+                {
+                    statusText.text = "Your turn";
+                    statusText.color = Color.yellow;
+                }
+                else {
+                    statusText.text = "Enemy turn";
+                    statusText.color = Color.gray;
+                }
+            }
+        }
+    }
+}
