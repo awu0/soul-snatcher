d75a374c6e4d495da2f142eaa59ffd6582924ce2
awu0 <82164146+awu0@users.noreply.github.com>
2024-11-30 18:39:09 -0500

    deleted old EyeLaser.cs code

--- Assets/Scripts/Abilities/EyeLaser.cs
+++ Assets/Scripts/Abilities/EyeLaser.cs
@@ -29,29 +29,4 @@ public class EyeLaser : Ability
             current += directionalContext.Direction;
         }
     }
-
-    /// <summary>
-    /// Damages every entity in the given direction.
-    /// </summary>
-    /// <param name="grids">The Grids object</param>
-    /// <param name="direction">The direction of damage: Vector2Int.up, Vector2Int.down, Vector2Int.left, Vector2Int.right</param>
-    // public void ActivateAbility(Grids grids, Vector2Int direction)
-    // {
-    //     Debug.Log($"{gameObject} used EyeLaser");
-        
-    //     // moves continuously in the specified direction until out of bounds
-    //     var (startX, startY) = Caster.GetCurrentPosition();
-        
-    //     Vector2Int current = new Vector2Int(startX, startY) + direction;
-        
-    //     while (grids.IsPositionWithinBounds(current.x, current.y))
-    //     {
-    //         Entity entity = grids.GetEntityAt(current.x, current.y);
-    //         if (entity != null)
-    //         {
-    //             entity.TakeDamage(damage);     
-    //         }
-    //         current += direction;
-    //     }
-    // }
 }
